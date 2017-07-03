182f1259088bb1c97c8dea9d432f5f200dd438be
Sarm Boonlertuthai <sarm333@gmail.com>
2017-07-03 16:20:56 +0100

    project initial commit

---
+++ src/test/java/busuu/AbstractTestCase.java
@@ -0,0 +1,21 @@
+package busuu;
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.*;
+
+import java.lang.reflect.Method;
+
+public class AbstractTestCase {
+
+    protected final Logger logger = Logger.getLogger(getClass());
+
+    @BeforeMethod
+    public void beforeMethod(Method method) {
+        logger.info("Executing Test Case: [" + method.getName() + "]"); ;
+    }
+
+    @AfterMethod
+    public void afterMethod(Method method) {
+        logger.info("Ending Test Case: [" + method.getName() + "]"); ;
+    }
+}
